9e432baa73ecd612ba3e755acd998327ae3b8e8a
FakhriMalpu <62053775+FakhriMalpu@users.noreply.github.com>
2022-12-11 00:23:54 +0700

    Initial commit

---
+++ hi-ava-master/assets/quotes.js
@@ -0,0 +1,10 @@
+// prettier-ignore
+export default [
+    { author: "FakhriMalpu", quotes: "If loving you was a job, I'd be the most deserving, dedicated, and qualified candidate. In fact, I'd even be willing to work for free!" },
+    // { author: "FakhriMalpu", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "FakhriMalpu", quotes: "Thank you for always being my rainbow after the storm." },
+    { author: "FakhriMalpu", quotes: "I love you." },
+    { author: "FakhriMalpu", quotes: "I can’t explain the way you make me feel when I hear your voice or see your face, but I love it." },
+    // { author: "FakhriMalpu", quotes: "I love you." },
+    // { author: "FakhriMalpu", quotes: "" },
+];
---
+++ hi-ava-master/assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["Ajeng 🖤", "Minnie 🖤", "Cantik 🖤", "Sayang 🖤",],
+  greetings: {
+    evening: "Good Evening",
+    afternoon: "Good Afternoon",
+    day: "Great Day",
+    morning: "Selamat Pagii",
+    night: "Good Night"
+  }
+};
